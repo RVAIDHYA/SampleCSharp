2d669b23f086136a16b6736ec54c63bfb260a082
Revathi Vaidhyanathan <revathi_v212@hotmail.com>
2020-12-11 14:54:28 -0500

    Add project files.

---
+++ SampleCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SampleCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Reverse string
+            ReverseString reverseString = new ReverseString();
+            reverseString.Execute();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SampleCSharp/ReverseString.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SampleCSharp
+{
+    public class ReverseString
+    {
+        public void Execute()
+        {
+            // the string to be reversed
+            string sampleString = "one two three four five six";
+
+            //expected string output - "eno owt eerht ruof evif xis"
+
+            //normal way
+            string[] strArray = sampleString.Split(" ");
+
+            string resultString = "";
+
+            foreach(string x in strArray)
+            {
+                resultString = resultString + " " + new String(x.Reverse().ToArray());
+            }
+
+
+
+            // in one line
+            //string resultString = string.Join(" ", sampleString.Split(' ').Select(x => new String(x.Reverse().ToArray())));
+
+            Console.WriteLine(resultString);
+
+
+
+        }
+    }
+}
